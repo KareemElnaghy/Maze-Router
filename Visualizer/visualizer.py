b250c49c014abc4320b123d5d6f642eecbd5db4f
Ahmed Waseem <ahmedwaseemr@aucegypt.edu>
2025-05-10 16:06:12 +0300

    preliminary visualization

--- Visualizer/visualizer.py
+++ Visualizer/visualizer.py
@@ -4,13 +4,17 @@ from PyQt6 import QtWidgets
 from vispy.scene import SceneCanvas, visuals
 from vispy.app import use_app
 
-IMAGE_SHAPE = (600, 800)  # (height, width)
-CANVAS_SIZE = (800, 600)  # (width, height)
-NUM_LINE_POINTS = 200
+import algorithm
+from algorithm import lee_router
 
-COLORMAP_CHOICES = ["viridis", "reds", "blues"]
-LINE_COLOR_CHOICES = ["black", "red", "blue"]
+CANVAS_SIZE = (800, 950)  # (width, height)
 
+GRID1 = np.linspace(0, 128, num=10*10, dtype=np.float32).reshape((10, 10))
+GRID2 = np.linspace(255, 0, num=10*10, dtype=np.float32).reshape((10, 10))
+IMAGE_SHAPE = GRID1.shape
+
+COLORMAP_CHOICES = ["grays", "reds", "blues", "viridis"]
+LAYER_CHOICES = ["1", "2"]
 
 class MyMainWindow(QtWidgets.QMainWindow):
     def __init__(self, *args, **kwargs):
@@ -31,7 +35,6 @@ class MyMainWindow(QtWidgets.QMainWindow):
 
     def _connect_controls(self):
         self._controls.colormap_chooser.currentTextChanged.connect(self._canvas_wrapper.set_image_colormap)
-        self._controls.line_color_chooser.currentTextChanged.connect(self._canvas_wrapper.set_line_color)
 
 
 class Controls(QtWidgets.QWidget):
@@ -44,23 +47,23 @@ class Controls(QtWidgets.QWidget):
         self.colormap_chooser.addItems(COLORMAP_CHOICES)
         layout.addWidget(self.colormap_chooser)
 
-        self.line_color_label = QtWidgets.QLabel("Line color:")
-        layout.addWidget(self.line_color_label)
-        self.line_color_chooser = QtWidgets.QComboBox()
-        self.line_color_chooser.addItems(LINE_COLOR_CHOICES)
-        layout.addWidget(self.line_color_chooser)
+        self.layer_label = QtWidgets.QLabel("Layer Choice:")
+        layout.addWidget(self.layer_label)
+        self.layer_chooser = QtWidgets.QComboBox()
+        self.layer_chooser.addItems(LAYER_CHOICES)
+        layout.addWidget(self.layer_chooser)
 
         layout.addStretch(1)
         self.setLayout(layout)
-
 
 class CanvasWrapper:
     def __init__(self):
         self.canvas = SceneCanvas(size=CANVAS_SIZE)
         self.grid = self.canvas.central_widget.add_grid()
 
-        self.view_top = self.grid.add_view(0, 0, bgcolor='cyan')
-        image_data = _generate_random_image_data(IMAGE_SHAPE)
+        self.view_top = self.grid.add_view(0, 0, bgcolor='gray')
+        #image_data = _generate_grid(IMAGE_SHAPE)
+        image_data = _get_lee_router_path()
         self.image = visuals.Image(
             image_data,
             texture_format="auto",
@@ -70,33 +73,39 @@ class CanvasWrapper:
         self.view_top.camera = "panzoom"
         self.view_top.camera.set_range(x=(0, IMAGE_SHAPE[1]), y=(0, IMAGE_SHAPE[0]), margin=0)
 
-        self.view_bot = self.grid.add_view(1, 0, bgcolor='#c0c0c0')
-        line_data = _generate_random_line_positions(NUM_LINE_POINTS)
-        self.line = visuals.Line(line_data, parent=self.view_bot.scene, color=LINE_COLOR_CHOICES[0])
-        self.view_bot.camera = "panzoom"
-        self.view_bot.camera.set_range(x=(0, NUM_LINE_POINTS), y=(0, 1))
-
     def set_image_colormap(self, cmap_name: str):
         print(f"Changing image colormap to {cmap_name}")
         self.image.cmap = cmap_name
 
-    def set_line_color(self, color):
-        print(f"Changing line color to {color}")
-        self.line.set_data(color=color)
+def _get_lee_router_path():
+    grid = [
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    ]
 
+    pins = [(1, 1), (2, 4), (5, 2)]
+    path = np.array(lee_router(grid, pins), np.float32)
+    gridnp = np.array(grid, np.float32)
+    for y, x in path.astype(int):
+        gridnp[y, x] = 255
+
+    IMAGE_SHAPE = gridnp.shape
+    return gridnp
 
 def _generate_random_image_data(shape, dtype=np.float32):
     rng = np.random.default_rng()
     data = rng.random(shape, dtype=dtype)
     return data
 
-
-def _generate_random_line_positions(num_points, dtype=np.float32):
-    rng = np.random.default_rng()
-    pos = np.empty((num_points, 2), dtype=np.float32)
-    pos[:, 0] = np.arange(num_points)
-    pos[:, 1] = rng.random((num_points,), dtype=dtype)
-    return pos
+def _generate_grid(shape, dtype=np.float32):
+    return GRID1;
 
 if __name__ == "__main__":
     app = use_app("pyqt6")
